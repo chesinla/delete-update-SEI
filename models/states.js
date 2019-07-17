627dc826bf36490650ab12dbedb4e2509bf92ee0
CT <ct777@CTs-MacBook-Pro.local>
2019-07-17 13:14:07 -0700

    added array of crudables

---
+++ models/states.js
@@ -0,0 +1,23 @@
+const states = [
+{ 
+    name: "California",  
+    capital: "Sacramento"
+},
+{
+    name:"Kentucky",
+    capital:"Frankfort"
+},
+{
+    name: "Texas",
+    capital:"Austin"
+},
+{
+    name: "New York",
+    capital:"Albany"
+},
+{
+    name: "Hawaii",
+    capital: "Honolulu"
+},
+]
+  
